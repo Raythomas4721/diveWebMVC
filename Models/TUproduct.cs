4d6abbdbf67459aafebb788f2dea5360002b77c2
Chloe <xian19991023@gmail.com>
2025-01-10 12:14:54 +0800

    add

--- Models/TUproduct.cs
+++ Models/TUproduct.cs
@@ -2,35 +2,48 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace diveWebMVC.Models;
 
 public partial class TUproduct
 {
+    [Display(Name = "商品編號")]
+    [Required(ErrorMessage = "商品編號未填寫")]
     public int ProductId { get; set; }
-
+    [Display(Name = "賣家編號")]
+    [Required(ErrorMessage = "賣家編號未填寫")]
     public int? SellerId { get; set; }
-
+    [Display(Name = "分類編號")]
+    [Required(ErrorMessage = "分類編號未填寫")]
     public int? CategoryId { get; set; }
-
+    [Display(Name = "商品名稱")]
+    [Required(ErrorMessage = "商品名稱未填寫")]
     public string ProductName { get; set; }
-
+    [Display(Name = "商品描述")]
+    [Required(ErrorMessage = "商品描述未填寫")]
     public string ProductDescription { get; set; }
-
+    [Display(Name = "商品價格")]
+    [Required(ErrorMessage = "商品價格未填寫")]
     public decimal? ProductPrice { get; set; }
-
+    [Display(Name = "更新時間")]
     public DateTime? UpdatedAt { get; set; }
-
+    [Display(Name = "建立時間")]
+    [Required(ErrorMessage = "建立時間未填寫")]
     public DateTime? CreatedAt { get; set; }
-
+    [Display(Name = "商品狀態編號")]
+    [Required(ErrorMessage = "商品狀態編號未填寫")]
     public int? ProductConditionId { get; set; }
-
+    [Display(Name = "商品狀態")]
+    [Required(ErrorMessage = "商品狀態未填寫")]
     public bool? ProductStatus { get; set; }
-
+    [Display(Name = "商品分類")]
+    [Required(ErrorMessage = "商品分類未填寫")]
     public virtual TUcategory Category { get; set; }
-
+    [Display(Name = "商品狀態")]
+    [Required(ErrorMessage = "商品狀態未填寫")]
     public virtual TUproductCondition ProductCondition { get; set; }
-
+    [Display(Name = "賣家資訊")]
     public virtual TMmemberList Seller { get; set; }
 
     public virtual ICollection<TUorderDetail> TUorderDetails { get; set; } = new List<TUorderDetail>();
